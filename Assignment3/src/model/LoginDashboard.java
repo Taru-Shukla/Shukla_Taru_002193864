9fa17ef725b2857c1769a0f3d3b25fb5c15228db
Taru Shukla <shukla.t@northeastern.edu>
2021-10-24 22:49:35 -0400

    initial commit for Assignment 3

---
+++ Assignment3/src/model/LoginDashboard.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author NetrA
+ */
+public class LoginDashboard {
+    String student;
+    Integer id;
+
+    public String getStudent() {
+        return student;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    
+    
+}
