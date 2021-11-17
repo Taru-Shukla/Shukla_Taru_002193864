c197a51a6b130822967157e632b56290d319d9d1
Taru Shukla <shukla.t@northeastern.edu>
2021-11-16 23:19:50 -0500

    added code in Business.Delivery

--- Assignment5/db4odemo/src/Business/DeliveryMan/DeliveryMan.java
+++ Assignment5/db4odemo/src/Business/DeliveryMan/DeliveryMan.java
@@ -5,10 +5,61 @@
  */
 package Business.DeliveryMan;
 
+
+
 /**
  *
  * @author harold
+ * @author taru shukla
  */
 public class DeliveryMan {
-    
+
+    private String deliveryManName;
+    private String username;
+    private String password;
+    private Boolean available;
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public String getDeliveryManName() {
+        return deliveryManName;
+    }
+
+    public void setDeliveryManName(String deliveryManName) {
+        this.deliveryManName = deliveryManName;
+
+    }
+
+    public boolean isAvailability() {
+        return available;
+    }
+
+    public DeliveryMan(String deliveryManName, String username, String password) {
+        this.deliveryManName = deliveryManName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
--- Assignment5/db4odemo/src/Business/DeliveryMan/DeliveryManDirectory.java
+++ Assignment5/db4odemo/src/Business/DeliveryMan/DeliveryManDirectory.java
@@ -5,10 +5,51 @@
  */
 package Business.DeliveryMan;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author harold
+ * @author taru shukla
  */
 public class DeliveryManDirectory {
+    private ArrayList<DeliveryMan> deliveryManDir;
+    private DeliveryMan d;
+
+    public DeliveryManDirectory() {
+        deliveryManDir = new ArrayList<>();
+    }
+
+    public ArrayList<DeliveryMan> getDeliveryManDir() {
+        return deliveryManDir;
+    }
+
+    public DeliveryMan getD() {
+        return d;
+    }
+
+    public DeliveryMan setD(DeliveryMan d) {
+        this.d = d;
+        deliveryManDir.add(d);
+        return d;
+    }
+    
+    public DeliveryMan findDeliveryManByName(String name){
+       for(DeliveryMan d: deliveryManDir){
+           if(d.getUsername().equals(name)){
+               return d;
+           }
+       } 
+       return null;
+    }
+    public String deleteDeliveryManByName(String name){
+       for(DeliveryMan d: deliveryManDir){
+           if(d.getUsername().equals(name)){
+               deliveryManDir.remove(d);
+               return "Account deleted successfully";
+           }
+       }
+       return "User not found";
+    }
     
 }
